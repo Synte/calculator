bbde5c54b1c39cedcef2e589ec2a0603f5f05c7e
Emma <aryyil2@gmail.com>
2019-10-04 22:30:55 +0100

    add basic calculator functions

--- script.js
+++ script.js
@@ -1,11 +1,113 @@
 //null, numberA, numberB, operator, equals, point
 
-function clickNumber() {}
-function clickOperator() {}
-function clickEquals() {}
-function clickClear() {}
-function clickPoint() {}
-function clickBack() {}
+function clickNumber() {
+    let numStr = this.value;
+
+    switch (lastInputType) {
+        case null:
+        case "equals":
+            if (numStr === "0") return;
+            screenPara.textContent = numStr;
+            numberA = Number(numStr);
+            lastInputType = "numberA";
+            break;
+        case "numberA":
+            screenPara.textContent += numStr;
+            numberA = Number(screenPara.textContent);
+            lastInputType = "numberA";
+            break;
+        case "numberB":
+            screenPara.textContent += numStr;
+            numberB = Number(screenPara.textContent.match(regex)[2]);
+            lastInputType = "numberB";
+            break;
+        case "operator":
+            if (numStr === "0") return;
+            screenPara.textContent += numStr;
+            numberB = Number(numStr);
+            lastInputType = "numberB";
+            break;
+    }
+}
+
+function clickOperator() {
+    let opStr = " " + this.textContent + " ";
+    let opFn = ops[this.id];
+
+    switch (lastInputType) {
+        case null:
+            numberA = 0;
+            screenPara.textContent = "0" + opStr;
+            break;
+        case "numberA":
+            screenPara.textContent += opStr;
+            break;
+        case "numberB":
+            numberA = equals(numberA, numberB, activeOperator);
+            // fallthrough;
+        case "operator":
+            screenPara.textContent = numberA + opStr;
+            break;
+        case "equals":
+            numberA = Number(screenPara.textContent);
+            screenPara.textContent = numberA + opStr;
+            break;
+    }
+
+    activeOperator = opFn;
+    lastInputType = "operator";
+}
+
+function clickEquals() {
+    switch (lastInputType) {
+        case null:
+        case "numberA":
+            return;
+        case "operator":
+            numberB = numberA;
+            break;
+        case "equals":
+            numberA = Number(screenPara.textContent);
+            break;
+    }
+
+    // case "numberB":
+    screenPara.textContent = equals(numberA, numberB, activeOperator);
+    lastInputType = "equals";
+}
+
+function clickClear() {
+    switch (lastInputType) {
+        case null:
+        case "numberA":
+        case "numberB":
+        case "operator":
+        case "equals":
+        case "point":
+    }
+}
+
+function clickPoint() {
+    switch (lastInputType) {
+        case null:
+        case "numberA":
+        case "numberB":
+        case "operator":
+        case "equals":
+        case "point":
+    }
+}
+
+function clickBack() {
+    switch (lastInputType) {
+        case null:
+        case "numberA":
+        case "numberB":
+        case "operator":
+        case "equals":
+        case "point":
+    }
+}
 
 /* Calculator Operation Functions */
 const multiply = (a, b) => a * b;
@@ -37,3 +139,12 @@ let numberA = null;
 let numberB = null;
 let activeOperator = null;
 let lastInputType = null;
+const regex = /[^\s]+/g;
+
+/* Special Object Literal */
+const ops = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+}
